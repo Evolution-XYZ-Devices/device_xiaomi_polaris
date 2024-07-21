8fb5484d0915d7738e593cbc11140bd530068269
Le Hong Duc <hongducthbk123@gmail.com>
2024-07-21 13:28:49 +0700

    polaris: Initialize Evolution X

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evolution \
     $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
